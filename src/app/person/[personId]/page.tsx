3b4bc046b39eafa962216bfa5343b3fad26ee586
aoirint <aoirint@gmail.com>
2023-08-05 19:49:21 +0900

    add niconico community list

--- src/app/person/[personId]/page.tsx
+++ src/app/person/[personId]/page.tsx
@@ -84,6 +84,32 @@ export default function PersonPage({
                 </TableBody>
               </Table>
             </Box>
+            <Typography variant="h5" component="h3" sx={{ mt: 3 }}>
+              ニコニコミュニティ
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>アカウント</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {person.personNiconicoCommunities.map(personNiconicoCommunity => ((
+                    <TableRow
+                      key={personNiconicoCommunity.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <Link href={`https://com.nicovideo.jp/community/co${personNiconicoCommunity.niconico_community_id}`}>
+                          {personNiconicoCommunity.name}
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  )))}
+                </TableBody>
+              </Table>
+            </Box>
           </>
         ) : ""}
       </Box>
