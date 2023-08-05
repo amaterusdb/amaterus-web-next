63a3509ddeb5e0637535c11c6e90603966062525
aoirint <aoirint@gmail.com>
2023-08-05 17:04:56 +0900

    add drawer_app_bar

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,11 +1,17 @@
 import Button from '@mui/material/Button'
+import DrawerAppBar from '@/components/drawer_app_bar'
+import { Box, Toolbar } from '@mui/material'
 
 export default function Rooms() {
   return (
-    <main>
-      <div>
-        <Button variant="contained">Hello World</Button>
-      </div>
-    </main>
+    <>
+      <DrawerAppBar />
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <div>
+          <Button variant="contained">Hello World</Button>
+        </div>
+      </Box>
+    </>
   )
 }
