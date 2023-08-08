791896f7815c0bde1c369670f4789cb9a9391d7c
aoirint <aoirint@gmail.com>
2023-08-08 15:07:17 +0900

    match list dummy page

--- src/pages/match/index.tsx
+++ src/pages/match/index.tsx
@@ -2,7 +2,7 @@ import { Box, Toolbar, Typography, Link as MuiLink, Breadcrumbs } from '@mui/mat
 import NextLink from 'next/link'
 import DrawerAppBar from '@/components/drawer_app_bar'
 
-export default function PersonListPage() {
+export default function MatchListPage() {
   return (
     <>
       <DrawerAppBar />
@@ -14,10 +14,10 @@ export default function PersonListPage() {
               Amaterus
             </MuiLink>
           </NextLink>
-          <Typography color='text.primary'>人物リスト</Typography>
+          <Typography color='text.primary'>試合リスト</Typography>
         </Breadcrumbs>
         <Typography variant='h4' component='h2' sx={{ mt: 2 }}>
-          人物リスト
+          試合リスト
         </Typography>
         <Typography sx={{ mt: 2 }}>未実装</Typography>
       </Box>
