2aa291a6a16fd3ade2247e6d63bcf974b66a7c50
aoirint <aoirint@gmail.com>
2023-08-08 15:09:16 +0900

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>TBW</main>
+import { Box, Toolbar, Typography, Link as MuiLink, Breadcrumbs } from '@mui/material'
+import NextLink from 'next/link'
+import DrawerAppBar from '@/components/drawer_app_bar'
+
+export default function HomePage() {
+  return (
+    <>
+      <DrawerAppBar />
+      <Box component='main' sx={{ p: 3 }}>
+        <Toolbar />
+        <Breadcrumbs aria-label='breadcrumb'>
+          <Typography color='text.primary'>Amaterus</Typography>
+        </Breadcrumbs>
+        <Typography variant='h4' component='h2' sx={{ mt: 2 }}>
+          Amaterus
+        </Typography>
+        <Typography sx={{ mt: 2 }}>
+          <NextLink href='/room/' passHref legacyBehavior>
+            <MuiLink>部屋リスト</MuiLink>
+          </NextLink>
+        </Typography>
+      </Box>
+    </>
+  )
 }
