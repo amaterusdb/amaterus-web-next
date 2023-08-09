d1c35745df4383c62b2cf6739b742ed1a423f2c1
aoirint <aoirint@gmail.com>
2023-08-10 02:01:22 +0900

    dark mode depends on system preference

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,36 @@
 import '@/styles/globals.css'
 import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline, createTheme, useMediaQuery } from '@mui/material'
 import type { AppProps } from 'next/app'
+import React from 'react'
 import GoogleAnalytics from '@/components/google_analytics'
 import { createApolloClient } from '@/lib/apollo'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  )
+
   const client = createApolloClient()
 
   return (
     <>
-      <GoogleAnalytics />
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GoogleAnalytics />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
     </>
   )
 }
