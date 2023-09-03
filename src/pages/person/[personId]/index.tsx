ea71f4a1bd1025da8378250a610a0e4c245f1337
aoirint <aoirint@gmail.com>
2023-09-03 14:49:08 +0900

    person page: niconico channel plus

--- src/pages/person/[personId]/index.tsx
+++ src/pages/person/[personId]/index.tsx
@@ -292,6 +292,42 @@ export default function PersonPage({ personId }: { personId: string }) {
         ) : (
           ''
         )}
+        {person.personNiconicoPlusChannels.length > 0 ? (
+          <>
+            <Typography variant='h5' component='h3' sx={{ mt: 3 }}>
+              ニコニコチャンネル+
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <Table sx={{ minWidth: 650 }} size='small' aria-label='simple table'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>チャンネル</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {person.personNiconicoPlusChannels.map((personNiconicoPlusChannel) => (
+                    <TableRow
+                      key={personNiconicoPlusChannel.niconicoPlusChannel.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component='th' scope='row'>
+                        <NextLink
+                          href={`https://nicochannel.jp/${personNiconicoPlusChannel.niconicoPlusChannel.screenName}/`}
+                          passHref
+                          legacyBehavior
+                        >
+                          <MuiLink>{personNiconicoPlusChannel.niconicoPlusChannel.name}</MuiLink>
+                        </NextLink>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </>
+        ) : (
+          ''
+        )}
         {person.personYoutubeChannels.length > 0 ? (
           <>
             <Typography variant='h5' component='h3' sx={{ mt: 3 }}>
