9defea700e9df3309f76fb448ac8cf7865154a65
aoirint <aoirint@gmail.com>
2023-09-20 20:36:30 +0900

    program page: among us vr matches

--- src/pages/program/[programId]/index.tsx
+++ src/pages/program/[programId]/index.tsx
@@ -274,6 +274,61 @@ export default function ProgramPage({ programId }: { programId: string }) {
         ) : (
           ''
         )}
+        {program.amongusvrMatches.length > 0 ? (
+          <>
+            <Typography variant='h5' component='h3' sx={{ mt: 3 }}>
+              Among Us VR
+            </Typography>
+            <Typography variant='h6' component='h4' sx={{ mt: 1 }}>
+              試合
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <Table sx={{ minWidth: 650 }} size='small' aria-label='simple table'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>番号</TableCell>
+                    <TableCell>開始日時</TableCell>
+                    <TableCell>終了日時</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {program.amongusvrMatches.map((amongusvrMatch, index) => (
+                    <TableRow
+                      key={amongusvrMatch.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>
+                        {amongusvrMatch.startTime != null ? (
+                          <NextLink
+                            href={`/amongusvr_match/${amongusvrMatch.id}/`}
+                            passHref
+                            legacyBehavior
+                          >
+                            <MuiLink>
+                              {format(parseISO(amongusvrMatch.startTime), 'yyyy-MM-dd HH:mm:ss')}
+                            </MuiLink>
+                          </NextLink>
+                        ) : (
+                          ''
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {amongusvrMatch.endTime != null ? (
+                          <>{format(parseISO(amongusvrMatch.endTime), 'yyyy-MM-dd HH:mm:ss')}</>
+                        ) : (
+                          ''
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </>
+        ) : (
+          ''
+        )}
         {program.fallguysMatches.length > 0 ? (
           <>
             <Typography variant='h5' component='h3' sx={{ mt: 3 }}>
