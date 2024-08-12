606e02ff8c251906e2d13944d6330adc0161e032
aoirint <aoirint@gmail.com>
2024-08-12 18:36:47 +0900

    codegen config: Use typescript (#10)
    * codegen config: Use typescript
    
    * improve type check
    
    * load_schema funciton
    
    * npm run generate: use codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,31 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
 const NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL =
   process.env.NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL
 const AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET = process.env.AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET
 
-module.exports = {
-  schema: [
-    {
-      [NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL]: {
-        headers: {
-          'X-Hasura-Admin-Secret': AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET,
-        },
-      },
+function load_schema(): CodegenConfig["schema"] {
+  if (NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL === undefined) {
+    return [];
+  }
+
+  const options = AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET !== undefined ? {
+    headers: {
+      'X-Hasura-Admin-Secret': AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET,
     },
-  ],
+  } : {};
+
+  return [
+    {
+      [NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL]: options,
+    },
+  ]
+}
+
+const schema = load_schema();
+
+const config: CodegenConfig = {
+  schema,
   documents: ['./src/**/*.graphql'],
   overwrite: true,
   generates: {
@@ -29,3 +43,5 @@ module.exports = {
     },
   },
 }
+
+export default config;
