d5579309078b64462a47413904e051df7bc9f715
aoirint <aoirint@gmail.com>
2024-08-12 18:34:57 +0900

    load_schema funciton

--- codegen.ts
+++ codegen.ts
@@ -4,17 +4,25 @@ const NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL =
   process.env.NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL
 const AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET = process.env.AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET
 
-const schema: CodegenConfig["schema"] = NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL !== undefined ? (
-  [
+function load_schema(): CodegenConfig["schema"] {
+  if (NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL === undefined) {
+    return [];
+  }
+
+  const options = AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET !== undefined ? {
+    headers: {
+      'X-Hasura-Admin-Secret': AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET,
+    },
+  } : {};
+
+  return [
     {
-      [NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL]: AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET !== undefined ? {
-        headers: {
-          'X-Hasura-Admin-Secret': AMATERUS_HASURA_GRAPHQL_ADMIN_SECRET,
-        },
-      } : {},
+      [NEXT_PUBLIC_AMATERUS_HASURA_GRAPHQL_SCHEMA_URL]: options,
     },
   ]
-) : {};
+}
+
+const schema = load_schema();
 
 const config: CodegenConfig = {
   schema,
